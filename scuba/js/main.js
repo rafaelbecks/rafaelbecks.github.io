d685ebc212788a3ca50dafae245eadec61642a1e
Rafael <rafaelbecks93@gmail.com>
2015-07-01 01:38:46 -0430

    diez mil fixes

--- scuba/js/main.js
+++ scuba/js/main.js
@@ -48,3 +48,19 @@ function validarEntero(e){
     te = String.fromCharCode(tecla);
     return patron.test(te);
 }
+
+function detectmob() { 
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
+    return true;
+  }
+ else {
+    return false;
+  }
+}
