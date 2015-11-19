4e1a606d36d7fd48aa39d481c82353bc0546ab1a
Rafael <rafaelbecks93@gmail.com>
2015-11-19 14:18:37 -0430

    graph search fucking working!

---
+++ js/main.js
@@ -0,0 +1,102 @@
+if(document.URL.indexOf("#")==-1){
+  location.href="#/";
+}
+
+var scuba_site=angular.module('scuba_site',['ui.router']);
+
+scuba_site.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+      //$urlRouterProvider.otherwise('/404');
+        $stateProvider
+            .state('main', {
+                url: '/',
+                templateUrl : 'home.html'
+            })
+            .state('cursos',{
+            	url: '/cursos',
+            	templateUrl: 'cursos.html'
+            })
+
+            .state('actividades',{
+            	url: '/actividades',
+            	templateUrl: 'actividades.html'
+            })
+
+            .state('tienda',{
+            	url: '/tienda',
+            	templateUrl: 'tienda.html'
+            })
+
+            .state('contacto',{
+            	url: '/contacto',
+            	templateUrl: 'contacto.html'
+            })
+            .state('staff',{
+                url: '/staff',
+                templateUrl: 'staff.html'
+            })
+            .state('eventos',{
+                url: '/eventos',
+                templateUrl: 'eventos.html'
+            })  
+
+});
+
+
+function validarEntero(e){
+    tecla = (document.all) ? e.keyCode : e.which;
+    patron =/[\d]/;
+    te = String.fromCharCode(tecla);
+    return patron.test(te);
+}
+
+function detectmob() { 
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
+    return true;
+  }
+ else {
+    return false;
+  }
+}
+
+scuba_site.controller('cursosController', ['$scope', function($scope){
+
+    $scope.templates=
+    [ { name: 'discover.html', url: 'cursos/discover.html'},
+    {name: 'open_water.html', url: 'cursos/open_water.html'},
+    {name: 'advanced_open.html', url: 'cursos/advanced_open.html'},
+    {name: 'navegacion_sub.html', url: 'cursos/navegacion_sub.html'},
+    {name: 'buceo_profundo.html', url: 'cursos/buceo_profundo.html'},
+    {name: 'aire_enriquecido.html', url: 'cursos/aire_enriquecido.html'},
+    {name: 'naufragios.html', url: 'cursos/naufragios.html'},
+    {name: 'multinivel.html', url: 'cursos/multinivel.html'},
+    {name: 'corrientes.html', url: 'cursos/corrientes.html'},
+    {name: 'busqueda.html', url: 'cursos/busqueda.html'},
+    {name: 'nocturno.html', url: 'cursos/nocturno.html'},
+    {name: 'desde_bote.html', url: 'cursos/desde_bote.html'},
+    {name: 'rescue_diver.html', url: 'cursos/rescue_diver.html'}
+    ];
+    $scope.curso=function(curso,titulo){
+      $scope.template=$scope.templates[curso];
+      $scope.comentario=titulo;
+    }
+    alet("AGC");
+    if(sessionStorage.curso==undefined || sessionStorage.titulo==undefined)
+        $scope.curso(1,"DISCOVER SCUBA DIVING");           
+    else
+        $scope.curso(sessionStorage.curso,sessionStorage.titulo);           
+    
+
+    $scope.href_curso=function(curso,titulo){
+        sessionStorage.curso=curso;
+        sessionStorage.titulo=titulo;
+        window.location.href="#/cursos";
+    }
+    
+}]);(window.angular);
--- prog_network/js/app/controllers.js
+++ prog_network/js/app/controllers.js
@@ -1,6 +1,11 @@
 progtonode.controller('mainController', function($scope ,$http, services){
 
 	$scope.searchArtist=function(keyword){
+
+ graph={
+  "nodes":[],
+  "links":[]
+};
 		$("html, body").animate({ scrollTop: $('#results').offset().top }, 1000);
 		location.href="#"+keyword;
 		services.searchMusic(keyword).then(function(data){
@@ -8,7 +13,14 @@ progtonode.controller('mainController', function($scope ,$http, services){
 			result=true_data.results[0].id;
 			$scope.img_artist=true_data.results[0].thumb;
 			services.getArtistInfo(result).then(function(data){
+				
 				$scope.artistBase=data.data.data;
+				
+				if($scope.artistBase.groups!=undefined)
+					buildGraph($scope.artistBase.name,$scope.artistBase.groups);
+				if($scope.artistBase.members!=undefined)
+					buildGraph($scope.artistBase.name,$scope.artistBase.members);				
+
 				var bio = $scope.artistBase.profile;
 				if(bio.indexOf("[a")>0){
 					first_reference=bio.substring(bio.indexOf("[a")+2,bio.indexOf("]"));
@@ -21,6 +33,20 @@ progtonode.controller('mainController', function($scope ,$http, services){
 		});
 
 	
+	};
+
+
+	buildGraph=function(name,groups){
+		graph.nodes.push({"name":name,"group":1});
+		for(var i=0;i<groups.length; i++){
+			if(groups[i].active)
+				graph.nodes.push({"name":groups[i].name,"group":1});
+			else
+				graph.nodes.push({"name":groups[i].name,"group":2});
+			graph.links.push({"source":0,"target":i+1,"value":1});
+		}
+		console.log(graph);
+		drawGraph(graph);
 	}
 });
 
